070257ec78b1efa51d8d674a5eb84ccc383cae24
dulanjala kalansuriya <k.a.c.dulanjala@gmail.com>
2019-02-04 02:43:07 +0530

    points per message -> 25

--- server-modules/index.js
+++ server-modules/index.js
@@ -248,7 +248,7 @@ function grade50Messages (alphabet, messages, privateData, hintsRequested, submi
   }
 
   if (decryptedMessages.length > 0) {
-    score = Math.max(0, (50 * decryptedMessages.length) - nHints);
+    score = Math.max(0, (25 * decryptedMessages.length) - nHints);
     message = `Vous avez correctement décrypté le(s) message(s) ${listOfNumToStr(decryptedMessages)}` + message;
   } else {
     message = 'Vous n\'avez décrypté aucun message.' + message;
